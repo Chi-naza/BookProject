bbd40330533148e9a69d076d1785ed2f1fd49986
Ugwuoke Chinaza <ugwuokechinazajames@gmail.com>
2022-03-24 23:29:07 +0100

    Fixed exposed infos

--- BookProject/settings.py
+++ BookProject/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)9fhb#vg99ja)2y@m!+&(_5g_6=q0mpj513u32xf5p!d0+vz0f'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
